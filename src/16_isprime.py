f865a33ff670e8d11c1458c28081a6a20d86c71c
Andrew Dillon <majik895@gmail.com>
2019-11-29 10:29:54 -0500

    classes.py

---
+++ src/16_isprime.py
@@ -0,0 +1,20 @@
+
+prime_numbers = 0
+
+
+def is_prime(x):
+    if x >= 2:
+        for y in range(2, x):
+            if not (x % y):
+                return False
+    else:
+        return False
+    return True
+
+
+for i in range(int(input("How many numbers you wish to check: "))):
+    if is_prime(i):
+        prime_numbers += 1
+        print(i)
+
+print("We found " + str(prime_numbers) + " prime numbers.")
